4cdc9d2115ad4fd1f122083f19e472dd2e1afa63
saymynameplsvip <anoschin.nikita2016@yandex.ru>
2025-10-28 02:16:28 +0300

    feat: новый url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL ?? "https://api.earmiss.ru";
+const BASE_URL = process.env.REACT_APP_API_URL ?? "https://d5d3kh6vigfafsdv1ukv.4b4k4pg5.apigw.yandexcloud.net";
 
 export const api = axios.create({
   baseURL: BASE_URL,
